d6867d0e1914ea21c35a888aa9018f9fe54e1f8c
neil <neilgit@neilpang.com>
2023-12-04 22:35:24 +0100

    home

--- hooks/postBuild.sh
+++ hooks/postBuild.sh
@@ -15,3 +15,17 @@ echo '=================== start ===='
 
 
 
+bootadm set-menu timeout=1
+
+
+svcadm disable sendmail
+
+cat /etc/auto_master | grep -v /home >auto.txt
+cat auto.txt >/etc/auto_master
+rm -f auto.txt
+
+automount -v
+svcadm restart autofs
+
+
+
